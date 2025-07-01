ff201c119ec471efcd9bf4ecb7ade93846e93047
Fernando-Miranda-Hurtado <wilmarfmh@gmail.com>
2025-06-30 18:06:11 -0700

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,9 @@
+import Button from '../components/Button'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gray-100">
+      <Button>Click Me</Button>
+    </main>
+  )
+}
